a04b387c05cd6ef1e9e26a4bf735d65eb5de4cc8
Alexander Sung <alexander.sung@dfki.de>
2022-11-11 08:51:22 +0100

    Fix bridge interface for using kwargs

--- up_bridge/bridge.py
+++ up_bridge/bridge.py
@@ -106,7 +106,8 @@ class Bridge:
         return self.create_fluent(
             function.__name__,
             annotations['return'],
-            OrderedDict((parameter_name, api_type) for parameter_name, api_type in list(annotations.items())[:-1]),
+            OrderedDict((parameter_name, api_type) for parameter_name, api_type in list(annotations.items())
+                if parameter_name != 'return'),
             callable=function,
         )
 
@@ -136,7 +137,8 @@ class Bridge:
             # Use signature's types, without its return type.
             OrderedDict(
                 (parameter_name, self.get_type(api_type))
-                for parameter_name, api_type in list((dict(signature, **kwargs) if signature else kwargs).items())[:-1]
+                for parameter_name, api_type in list((dict(signature, **kwargs) if signature else kwargs).items())
+                    if parameter_name != 'return'
             ),
         )
         self._actions[name] = action
