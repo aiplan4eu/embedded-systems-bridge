87e7360384a4f9ed0af04ed73b3ccfd3afe32331
Sebastian Stock <sebastian.stock@gmail.com>
2022-11-22 15:36:07 +0100

    Add primitive version of plexmo

---
+++ tests/test_monitor.py
@@ -0,0 +1,50 @@
+# Copyright 2022 Sebastian Stock, DFKI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from typing import Tuple
+from up_bridge.plexmo.monitor import SequentialPlanMonitor
+
+from unified_planning.plans.plan import ActionInstance
+from unified_planning.plans.sequential_plan import SequentialPlan
+from unified_planning.test.examples import get_example_problems
+from unified_planning.model import UPCOWState
+from unified_planning.engines import SequentialSimulator
+
+
+def get_example_plan() -> SequentialPlan:
+    example_problems = get_example_problems()
+    return example_problems["basic"].plan
+
+
+def succeeding_check_cb(action: ActionInstance) -> Tuple[bool, str]:
+    return True, "True"
+
+
+class TestSequentialMonitor(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls._example = get_example_problems()["hierarchical_blocks_world"]
+
+    def test_check_preconditions(self):
+        instance = self._example.plan.actions[0]
+        monitor = SequentialPlanMonitor(self._example.problem)
+        state = UPCOWState(self._example.problem.initial_values)
+        self.assertTrue(monitor.check_preconditions(instance, state)[0])
+
+        # after applying the first action, its preconditions are not fulfilled, anymore
+        simulator = SequentialSimulator(self._example.problem)
+        events = simulator.get_events(instance.action, instance.actual_parameters)
+        state = simulator.apply(events[0], state)
+        self.assertFalse(monitor.check_preconditions(instance, state)[0])
