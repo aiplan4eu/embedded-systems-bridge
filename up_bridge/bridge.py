ec8c0c5e38377eb714e1166f7cebcebc9e3b8cbf
Alexander Sung <alexander.sung@dfki.de>
2023-02-03 12:04:29 +0100

    Update copyright information

--- up_bridge/bridge.py
+++ up_bridge/bridge.py
@@ -1,4 +1,5 @@
-# Copyright 2022 Alexander Sung, DFKI
+# Copyright 2022, 2023 DFKI GmbH
+# Copyright 2023 LAAS-CNRS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Authors:
+# - Alexander Sung, DFKI
+# - Sebastian Stock, DFKI
+# - Selvakumar H S, LAAS-CNRS
+
 
 import itertools
 import sys
