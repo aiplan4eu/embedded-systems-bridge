f6d8ccfcd6f4c2a437ca99dfb7492ccdb671dfc1
franklinselva <franklinselva10@gmail.com>
2023-07-03 17:34:00 +0200

    update plan monitor definition

--- up_esb/plexmo/dispatcher.py
+++ up_esb/plexmo/dispatcher.py
@@ -22,7 +22,9 @@ import networkx as nx
 from unified_planning.plans import Plan
 
 from up_esb.execution import ActionExecutor, ActionResult
-from up_esb.status import ActionNodeStatus, ConditionStatus, DispatcherStatus
+from up_esb.status import ActionNodeStatus, ConditionStatus, DispatcherStatus, MonitorStatus
+
+from .monitor import PlanMonitor
 
 
 class PlanDispatcher:
@@ -36,8 +38,9 @@ class PlanDispatcher:
         self._dispatched_position = 0
         self._options = None
         self._executor = None
-        self._plan = None
         # TODO: Use the plan for plan repair, replanning, etc.
+        self._plan = None
+        self._monitor = None
 
     def execute_plan(self, plan: Plan, graph: nx.DiGraph, **options):
         """Execute the plan."""
@@ -46,25 +49,41 @@ class PlanDispatcher:
         self._dispatched_position = 0
         self._options = options
         self._plan = plan
+        self._monitor = PlanMonitor(graph)
+        self._monitor.status = MonitorStatus.STARTED
 
         self._executor = ActionExecutor(graph, options=options)
         self._executor = self._executor.get_executor(plan)
 
+        # TODO: Dispatch actions based on predecessors and successors.
         for node_id, node in self._graph.nodes(data=True):
+            self._monitor.status = MonitorStatus.IN_PROGRESS
+
             if node["action"] in ["start", "end"]:
+                self._monitor.update_action_status(node_id, ActionNodeStatus.SUCCEEDED)
                 continue
             result = self._executor.execute_action(node_id)
+            self._monitor.update_action_status(node_id, result.action_status)
+
+            if result.result is None:
+                self._monitor.update_action_status(node_id, result.action_status)
+            else:
+                self._monitor.status = MonitorStatus.FAILED
+                self._monitor.process_action_result(node_id, result)
 
             if self._check_result(result) is False:
                 # TODO: Replan
                 self._status = DispatcherStatus.FAILED
             else:
+                self._monitor.status = MonitorStatus.IN_PROGRESS
                 self._status = DispatcherStatus.IN_PROGRESS
 
         if self._status == DispatcherStatus.FAILED and self._replan_cb is not None:
             return False
 
         self._status = DispatcherStatus.FINISHED
+        self._monitor.status = MonitorStatus.FINISHED
+
         return True
 
     def _check_result(self, result: ActionResult):
--- up_esb/plexmo/monitor.py
+++ up_esb/plexmo/monitor.py
@@ -14,6 +14,7 @@
 """Monitoring of a SequentialPlan."""
 import networkx as nx
 
+from up_esb.execution import ActionResult
 from up_esb.status import ActionNodeStatus, MonitorStatus
 
 
@@ -24,6 +25,42 @@ class PlanMonitor:
         self._graph = executable_graph
         self._status = MonitorStatus.IDLE
         self._action_status = ActionNodeStatus.NOT_STARTED
+
+        # Preprocess the graph to remove the executable elements.
+        self._preprocess_graph()
+
+    @property
+    def status(self) -> MonitorStatus:
+        """Current status of the monitor."""
+        return self._status
+
+    @status.setter
+    def status(self, status: MonitorStatus) -> None:
+        """Set the status of the monitor."""
+        self._status = status
+
+    def _preprocess_graph(self) -> None:
+        """Preprocess graph to remove the executable elements."""
+
+        new_graph = nx.DiGraph()
+
+        for node_id, node in self._graph.nodes(data=True):
+            # Add the node.
+            new_graph.add_node(node_id)
+            new_graph.nodes[node_id]["processed"] = False
+            new_graph.nodes[node_id]["action"] = node["action"]
+            new_graph.nodes[node_id]["status"] = ActionNodeStatus.NOT_STARTED
+            new_graph.nodes[node_id]["node_name"] = node["node_name"]
+            new_graph.nodes[node_id]["result"] = ""
+
+            # Add the edges.
+            new_graph.add_edges_from(self._graph.edges(node_id))
+
+            # Predecessors and successors.
+            new_graph.nodes[node_id]["predecessors"] = list(self._graph.predecessors(node_id))
+            new_graph.nodes[node_id]["successors"] = list(self._graph.successors(node_id))
+
+        self._graph = new_graph
 
     def get_status(self) -> MonitorStatus:
         """Return the current status of the monitor."""
@@ -41,3 +78,40 @@ class PlanMonitor:
             if node["node_name"] == action_name:
                 return node["status"]
         return ActionNodeStatus.UNKNOWN
+
+    def update_action_status(self, node_id: int, status: ActionNodeStatus) -> None:
+        """Update the status of the given action."""
+        self._graph.nodes[node_id]["status"] = status
+
+    def process_action_result(self, node_id: int, result: ActionResult) -> None:
+        """Process the result of an action."""
+
+
+class ProcessActionResult:
+    """Process ActionResult from the executor."""
+
+    def __init__(self, graph: nx.DiGraph):
+        self._graph = graph
+
+    def is_action_succeeded(self, node_id: int):
+        """Check if the action result is already processed."""
+        return self._graph.nodes[node_id]["status"] == ActionNodeStatus.SUCCEEDED
+
+    def is_action_failed(self, node_id: int):
+        """Check if the action result is already processed."""
+        return self._graph.nodes[node_id]["status"] == ActionNodeStatus.FAILED
+
+    def is_action_unknown(self, node_id: int):
+        """Check if the action result is already processed."""
+        return self._graph.nodes[node_id]["status"] == ActionNodeStatus.UNKNOWN
+
+    def is_action_not_started(self, node_id: int):
+        """Check if the action result is already processed."""
+        return self._graph.nodes[node_id]["status"] == ActionNodeStatus.NOT_STARTED
+
+    def is_action_started(self, node_id: int):
+        """Check if the action result is already processed."""
+        return self._graph.nodes[node_id]["status"] == ActionNodeStatus.STARTED
+
+    def process_result(self, result: ActionResult, node_id: int) -> None:
+        """Process the result of an action."""
